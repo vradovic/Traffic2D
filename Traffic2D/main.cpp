888c2a06ec49ac26d369073e703bfede6137a323
Vladislav Radovic <vradovic522@gmail.com>
2024-12-01 13:12:22 +0100

    Initialize glew and glfw

--- Traffic2D/main.cpp
+++ Traffic2D/main.cpp
@@ -1,7 +1,51 @@
 #include <iostream>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE "Traffic2D"
 
 int main() {
-	std::cout << "Hello, world!" << std::endl;
+	if (!glfwInit()) {
+		std::cout << "GLFW init fail" << std::endl;
+		return 1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE, NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create window" << std::endl;
+		glfwTerminate();
+		return 2;
+	}
+
+	glfwMakeContextCurrent(window);
+
+	if (glewInit() != GLEW_OK) {
+		std::cout << "GLEW init fail" << std::endl;
+		return 3;
+	}
+
+	glClearColor(0.827, 0.827, 0.827, 1.0);
+
+	while (!glfwWindowShouldClose(window))
+	{
+		if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		{
+			glfwSetWindowShouldClose(window, GL_TRUE);
+		}
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
